4368988d6d95fdc62e144aee4142cc20eaaadc1b
Romero Hutapea <61641738+romerojhh@users.noreply.github.com>
2024-03-30 10:25:08 -0700

    new stuff

--- newData/cleanupData.py
+++ newData/cleanupData.py
@@ -2,22 +2,37 @@ import csv
 
 def main():
     print
-    openFile("./newData/DonaldTrump.csv")
+    a = extractCol("newData/DonaldTrump.csv", [1,5], "Donald Trump", [])
+    b = extractCol("newData/JoeBiden.csv", [0,3], "Joe Biden", a)
+    writeData("tweetAggregated", b)
 
-def openFile(filename: str):
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        count = 0
+
+def extractCol(filename: str, columns_to_keep: list[int], author_name: str, data: list):
+    count = 0
+    with open(filename) as file:
+        reader = csv.reader(file)
+        next(reader)
         for row in reader:
-            isValid = False
-            for col in row:
-                if (row[2] == 'f' and row[1] == col):
-                    print(row[0], col, sep=',', end='')
-                    isValid = True
+            # index 2 isRetweet for donald trump
+            if author_name == 'Donald Trump' and row[2] == 't':
+                continue
+            else:
+                new_row = [row[i] for i in columns_to_keep]
+                new_row.insert(0, author_name)
+                data.append(new_row)
+                count += 1
 
-            if isValid:       
-                print('\n')
-            count += 1
-            if count >= 10:
+            if count >= 1000:
                 break
+    return data
+
+def writeData(output_file_name: str, data: list):
+    # Open a new CSV file for writing
+    with open(f'newData/{output_file_name}.csv', 'w') as file:
+        writer = csv.writer(file)
+        header = ["tweet_author", 'tweet', "like_count"]
+        writer.writerow(header)
+        # Write the data rows
+        writer.writerows(data)
+
 main()
